8c1832f7ba98b9b2f23f15261b2d953ee521d106
Sebastian Hoehn <sebastian.hoehn@googlemail.com>
2011-09-07 19:57:16 +0200

    First steps

--- Database/Persist/Riak.hs
+++ Database/Persist/Riak.hs
@@ -1,1 +1,24 @@
- 
+{-# LANGUAGE GeneralizedNewtypeDeriving, MultiParamTypeClasses, FlexibleInstances #-}
+{-# LANGUAGE TypeSynonymInstances, PackageImports, RankNTypes, CPP #-}
+module Database.Persist.Riak
+     ( withRiakConn
+     , runRiakConn
+     , HostName
+     , module Database.Persist
+     ) where
+
+import Database.Persist
+import Database.Persist.Base
+
+import Network.Socket (HostName)
+import qualified Network.Riak.Connection.Pool as Pool
+
+-- Riak database library
+import Network.Riak
+import Network.Riak.Connection
+
+type ConnectionPool = Pool.Pool
+
+withRiakConn = undefined
+   
+runRiakConn = undefined
