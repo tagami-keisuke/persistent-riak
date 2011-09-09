d422d8e1be76667e51ca0b83a82d6e6984a5081c
Sebastian Hoehn <sebastian.hoehn@googlemail.com>
2011-09-09 19:09:24 +0200

    Some minor progress :-(

--- Test/InterfaceTest.hs
+++ Test/InterfaceTest.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving, MultiParamTypeClasses, FlexibleInstances #-}
-{-# LANGUAGE TypeSynonymInstances, PackageImports, RankNTypes, CPP #-}
+{-# LANGUAGE TypeSynonymInstances, PackageImports, RankNTypes, CPP, TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies, QuasiQuotes, GADTs #-}
+
 -- | Test implementation for typeclass functions for the riak persist backend
 module Test.InterfaceTest
 -- I want all of them in my console for debugging
@@ -18,11 +20,31 @@ import qualified Network.Riak as R
 import qualified Network.Riak.Connection as R
 import qualified Network.Riak.Connection.Pool as R
 import qualified Network.Riak.Protocol.ServerInfo as R
+import qualified Network.Riak.Content as Content
+
 
 import qualified Data.ByteString.Lazy.UTF8 as B
 
+import Database.Persist
+import Database.Persist.TH
+import Database.Persist.Base
+
 import System.IO.Unsafe
 import Data.Maybe (fromJust)
+import "aeson-native" Data.Aeson
+import Data.Map as M
+import Data.Time
+
+-- | PersistEntity for testing
+mkPersist sqlSettings [persist|
+Person
+   name String
+   age Int Maybe
+   created UTCTime default=now()
+|]
+
+make_person :: PersonGeneric backend
+make_person = Person {personName="Sebastian",  personAge=(Just 30), personCreated=(unsafePerformIO getCurrentTime)}
 
 riakTests :: IO [IO Spec]
 riakTests = describe "riak database connection"
@@ -36,18 +58,45 @@ backendImplTests = describe "riak backend implementation"
     (property prop_true)
   ]
 
+insert :: (PersistBackend b m, PersistEntity val) 
+         => val
+         -> b m (Key b val)
+insert record = undefined --do
+  --oid <- R.put 
+    where
+      -- put takes: bucket key mvclock content quorum (W) quorum (DW) returnBody?
+      --put_request = R.put (entityName recDef) key mvclock content quorum quorum True
+      --recDef = entityDef record
+      --content = Content.json fieldsToJson
+      
+
+persistValue2JSON v = 
+  zipWith (\f x -> (f, x)) getFieldNames getValues
+   where
+    getFieldNames = Prelude.map columnName (entityColumns $ entityDef v)
+    getValues = Prelude.map (toPersistValue) (toPersistFields v)
+-- encode $ toJSON $ Data.Map.fromList $ Prelude.map (\x -> (columnName x, columnType x)) (entityColumns $ entityDef make_person)
+
+columns2JSON c acc = (columnName c) : acc 
+
 db_host :: [Char]
 db_host = "127.0.0.1"
 
 db_port :: [Char]
 db_port = "8081"
 
+-- | Convenience function for opening the connection.
+--   I am still confused if this create will always create
+--   a new connection pool? TODO: find out what it does and
+--   how that can be integrated in the persist backend.
+open_connection_pool :: IO R.Pool
 open_connection_pool = do
   c_id <- R.makeClientID
   let client = R.Client {R.host=db_host, R.port=db_port, R.clientID=c_id}
   R.create client 10 50 1
 --  R.withConnection p $ R.getServerInfo
   
+is_server_info :: Bool
 is_server_info = 
   case si of
     R.ServerInfo n v -> ((B.toString $ fromJust $ v) == "0.14.2") && ((B.toString $ fromJust $ n) == "dev1@127.0.0.1")
